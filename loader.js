1d410289a27bb5dd823c364e673f88f812ec2529
a-domani <a-domani@outlook.com>
2026-07-21 03:56:56 +0900

    Delete loader.js

--- loader.js
+++
@@ -1,8 +0,0 @@
-async function loadComponent(id, file) {
-  const res = await fetch(file);
-  const html = await res.text();
-  document.getElementById(id).innerHTML = html;
-}
-
-loadComponent("header", "header.html");
-loadComponent("footer", "footer.html");
